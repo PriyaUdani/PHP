40d8cdd86640d4fac99d8c4004f087e817704138
PriyaUdani <155055136+PriyaUdani@users.noreply.github.com>
2024-02-21 11:22:52 +0530

    Add files via upload

---
+++ Dashboard/inputfile.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<style>
+table
+{
+	background-color:pink;
+}
+</style>
+<body>
+<form action="DASHBOARD.php" method="get">
+<table cellpadding="5" cellspacing="5" border="5" align="center">
+	<tr>
+	<td>Share something About Me</td>
+	<td><input type="text" name="aboutme" id="aboutme"></td>
+	</tr>
+	
+	<tr>
+	<td>Share Something About Your Skills</td>
+	<td><input type="text" name="aboutskills" id="aboutskills"></td>
+	</tr>
+	
+	<tr>
+	<td>Share Something About Your Projects</td>
+	<td><input type="text" name="aboutproject" id="aboutproject"></td>
+	</tr>
+	
+	<tr>
+	<td colspan="2" align="center"><input type="submit" id="submit"></td>
+	</tr>
+</table>
+</form>
+</body>
+</head>
+</html>
